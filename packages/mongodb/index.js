99c8b61a15b664d8768d9522154ee9cf10047436
max <max@enkidoo.ai>
2019-03-22 15:55:20 -0400

    (fix: test) ...

--- packages/mongodb/index.js
+++ packages/mongodb/index.js
@@ -39,7 +39,7 @@ module.exports.init = (options, callback = null) => {
         .then(
             ({ message, client }) => {
 
-                DEBUG({ 'message': result, 'client': client })
+                DEBUG({ 'message': message, 'client': client })
 
                 MongoClient = client
 
